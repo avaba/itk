90533e72e5164a518df33a8b018b02a7c39d9eef
Tom <34061215+x-JustOneMan-x@users.noreply.github.com>
2020-11-24 15:36:48 +0200

    Revert "Merge branch 'master' of https://github.com/avaba/itk"
    This reverts commit 8c4e850970e06ca5fb8a5135085e927b46128f73, reversing
    changes made to 5b12ff5aa2225d9dc16e86167c4f27c9f545605c.

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,6 +1,3 @@
 import "%modules%/header/header";
 import "%modules%/page-checkbox/page-checkbox";
-import "%modules%/tab/tab";
-import "%modules%/select/select";
-import "%modules%/page-calc/page-calc";
 import "%modules%/footer/footer";
