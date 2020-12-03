d8a0c70e7fbf26c6e4ac6c161a9248f912c09894
Tom <34061215+x-JustOneMan-x@users.noreply.github.com>
2020-12-03 14:39:07 +0200

    market

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	$('.header__bot-menu').click(function(){
 		$('.header__bot--submenu').stop(true, true).slideToggle();
 	})
+
+	$('.menu__title').click(function(){
+		if(window.innerWidth < 993) {
+			$('.menu__title').not($(this)).next('ul').slideUp();
+			$('.menu__title').not($(this)).removeClass('active');
+			$(this).next('ul').slideToggle();
+			$(this).toggleClass('active');
+		}
+	})
 })
