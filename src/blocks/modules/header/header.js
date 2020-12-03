4f52a266e41e1f744cf2eb7f156771f17a623e68
avaba <42552832+avaba@users.noreply.github.com>
2020-12-03 15:26:06 +0300

    Правки главная

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,5 +1,50 @@
-$(document).ready(function(){
-	$('.header__bot-menu').click(function(){
-		$('.header__bot--submenu').stop(true, true).slideToggle();
-	})
+$(document).ready(function () {
+  $('.header__bot-menu').click(function () {
+    $('.header__bot--submenu').stop(true, true).slideToggle()
+  })
+
+  $('img.svg').each(function () {
+    var $img = $(this)
+    var imgID = $img.attr('id')
+    var imgClass = $img.attr('class')
+    var imgURL = $img.attr('src')
+
+    jQuery.get(
+      imgURL,
+      function (data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg')
+
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID)
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass + ' replaced-svg')
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a')
+
+        // Replace image with new SVG
+        $img.replaceWith($svg)
+      },
+      'xml',
+    )
+  })
+
+  $('.helper__btn').click(function () {
+    $('.helper__hide').toggle()
+  })
+
+  $('#app_link_select_popup a').click(function () {
+    if ($(this).attr('data-type') == 'windows') {
+      window.location.href =
+        'https://download.teamviewer.com/download/TeamViewerQS.exe'
+    } else {
+      window.location.href =
+        'https://download.teamviewer.com/download/TeamViewerQS.dmg'
+    }
+  })
 })
