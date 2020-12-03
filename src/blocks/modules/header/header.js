a23a5b4e7ce348cf296371aadb5fc74af07c5e74
avaba <42552832+avaba@users.noreply.github.com>
2020-12-03 15:49:16 +0300

    header js

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,20 +1,15 @@
-$(document).ready(function(){
-	$('.header__bot-menu').click(function(){
-		$('.header__bot--submenu').stop(true, true).slideToggle();
-	})
-
-	$('.menu__title').click(function(){
-		if(window.innerWidth < 993) {
-			$('.menu__title').not($(this)).next('ul').slideUp();
-			$('.menu__title').not($(this)).removeClass('active');
-			$(this).next('ul').slideToggle();
-			$(this).toggleClass('active');
-		}
-	})
-})
 $(document).ready(function () {
   $('.header__bot-menu').click(function () {
     $('.header__bot--submenu').stop(true, true).slideToggle()
+  })
+
+  $('.menu__title').click(function () {
+    if (window.innerWidth < 993) {
+      $('.menu__title').not($(this)).next('ul').slideUp()
+      $('.menu__title').not($(this)).removeClass('active')
+      $(this).next('ul').slideToggle()
+      $(this).toggleClass('active')
+    }
   })
 
   $('img.svg').each(function () {
