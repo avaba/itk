132b79194478fdc5727727944eda3c0c730612f2
Alexandr <alex.develop777@gmail.com>
2020-12-21 12:21:29 +0200

    fx_1

--- src/blocks/modules/contacts/contacts.js
+++ src/blocks/modules/contacts/contacts.js
@@ -1,11 +1,12 @@
 $('.offices__li').on('click', (e) => {
-    let city = e.target.dataset.city;
+    let cityDOM = e.target;
+    let cityName = cityDOM.dataset.city;
 
     $('.contacts-block').each(function () {
-        let cityInfo = this.dataset.cityInfo
-        if (city === cityInfo) {
+        let cityInfo = this.dataset.cityInfo;
+        if (cityName === cityInfo) {
             $(this).addClass('contacts-block--active').siblings().removeClass('contacts-block--active')
+            $(cityDOM).addClass('offices__li--active').siblings().removeClass('offices__li--active')
         }
     })
-
-})
+});
